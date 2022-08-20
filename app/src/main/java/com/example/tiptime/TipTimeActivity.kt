24e6976690bc8b559eb432213e49dde8e4391184
lusiro <lusiro2001@students.amikom.ac.id>
2022-08-20 14:58:02 +0700

    commit challenge try and error for radio button activated

--- app/src/main/java/com/example/tiptime/TipTimeActivity.kt
+++ app/src/main/java/com/example/tiptime/TipTimeActivity.kt
@@ -3,6 +3,7 @@ package com.example.tiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.RadioButton
 
 class TipTimeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +15,20 @@ class TipTimeActivity : AppCompatActivity() {
 
         // FullScreen
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        val rbServiceAmazing: RadioButton = findViewById(R.id.rb_serviceAmazing)
+        val rbServiceGood: RadioButton = findViewById(R.id.rb_serviceGood)
+        val rbServiceOk: RadioButton = findViewById(R.id.rb_serviceOk)
+
+        if (rbServiceAmazing.isChecked){
+            rbServiceGood.isChecked = false
+            rbServiceOk.isChecked = false
+        }else if (rbServiceGood.isChecked){
+            rbServiceAmazing.isChecked = false
+            rbServiceOk.isChecked = false
+        }else if (rbServiceOk.isChecked){
+            rbServiceAmazing.isChecked = false
+            rbServiceGood.isChecked = false
+        }
     }
 }
